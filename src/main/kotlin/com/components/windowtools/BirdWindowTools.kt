5ef1e755e21244a6a37f9faf7c73da4210366c3d
Pierre Paquin <p.paquin1016@laposte.net>
2023-05-23 20:43:43 +0200

    :sparkles: feat: adding image on left window tools

---
+++ src/main/kotlin/com/components/windowtools/BirdWindowTools.kt
@@ -0,0 +1,98 @@
+package com.components.windowtools
+
+import com.intellij.openapi.editor.DisposableEditorPanel
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.psi.PsiManager
+import com.intellij.psi.PsiTreeChangeEvent
+import com.intellij.psi.PsiTreeChangeListener
+import javax.swing.ImageIcon
+import javax.swing.JLabel
+import javax.swing.JPanel
+
+class BirdWindowTools: ToolWindowFactory {
+
+
+    private lateinit var imageLabel: JLabel
+    private lateinit var panel: JPanel
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        panel = JPanel()
+        imageLabel = JLabel()
+        panel.add(imageLabel)
+        toolWindow.contentManager.addContent(
+                toolWindow.contentManager.factory.createContent(
+                        panel, "", false
+                )
+        )
+
+        val psiManager: PsiManager = PsiManager.getInstance(project)
+
+        val psiTreeChangeListener = object : PsiTreeChangeListener {
+            override fun beforeChildAddition(event: PsiTreeChangeEvent) {
+                updateImage(project, imageLabel)
+            }
+
+            override fun beforeChildRemoval(event: PsiTreeChangeEvent) {
+                updateImage(project, imageLabel)
+            }
+
+            override fun beforeChildReplacement(event: PsiTreeChangeEvent) {
+                updateImage(project, imageLabel)
+            }
+
+            override fun beforeChildMovement(event: PsiTreeChangeEvent) {
+                updateImage(project, imageLabel)
+            }
+
+            override fun beforeChildrenChange(event: PsiTreeChangeEvent) {
+                updateImage(project, imageLabel)
+            }
+
+            override fun beforePropertyChange(event: PsiTreeChangeEvent) {
+                updateImage(project, imageLabel)
+            }
+
+            override fun childAdded(event: PsiTreeChangeEvent) {
+                updateImage(project, imageLabel)
+            }
+
+            override fun childRemoved(event: PsiTreeChangeEvent) {
+                updateImage(project, imageLabel)
+            }
+
+            override fun childReplaced(event: PsiTreeChangeEvent) {
+                updateImage(project, imageLabel)
+            }
+
+            override fun childrenChanged(event: PsiTreeChangeEvent) {
+                updateImage(project, imageLabel)
+            }
+
+            override fun childMoved(event: PsiTreeChangeEvent) {
+                updateImage(project, imageLabel)
+            }
+
+            override fun propertyChanged(event: PsiTreeChangeEvent) {
+                updateImage(project, imageLabel)
+            }
+
+        }
+
+        psiManager.addPsiTreeChangeListener(psiTreeChangeListener)
+
+        updateImage(project, imageLabel)
+    }
+
+    private fun updateImage(project: Project, image: JLabel) {
+
+        val urlImage = javaClass.getResource("/assets/images/high_success.jpg")
+        val imageIcon = ImageIcon(urlImage)
+        image.icon = imageIcon
+
+        image.revalidate()
+        image.repaint()
+
+    }
+
+}
