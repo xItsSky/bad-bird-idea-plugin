8f14d62b556479cf5638f9d85477bc5b987ca769
xItsSky <emo.quentin@gmail.com>
2023-05-29 15:48:05 +0200

    :art: feat(BirdPanel): Code cleaning

---
+++ src/main/kotlin/com/badbird/components/icons/BirdIcons.kt
@@ -0,0 +1,8 @@
+package com.badbird.components.icons
+
+import com.intellij.openapi.util.IconLoader
+
+object BirdIcons {
+    @JvmField
+    val Bird = IconLoader.getIcon("/assets/images/icon.png", javaClass)
+}
